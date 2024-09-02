7032208f910063dc6c585b1558fff2a33a21df14
Nathanael <Nathanael.carauna@gmail.com>
2024-09-02 14:56:13 -0300

    feat: implement cash card creation

--- src/main/java/example/cashcard/CashCardController.java
+++ src/main/java/example/cashcard/CashCardController.java
@@ -1,5 +1,6 @@
 package example.cashcard;
 
+import java.net.URI;
 import java.util.Optional;
 
 import org.springframework.http.ResponseEntity;
@@ -7,15 +8,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.util.UriComponentsBuilder;
+
 
 @RestController
 @RequestMapping("/cashcards")
 public class CashCardController {
 
-    private final CashCardRepository cashCardRepository;
+    private final CashCardRepository cashCardRepository;    
 
     private CashCardController(CashCardRepository cashCardRepository){
-        this.cashCardRepository = cashCardRepository;
+        this.cashCardRepository = cashCardRepository;        
     }
 
     @GetMapping("/{requestId}")
@@ -26,4 +31,14 @@ public class CashCardController {
         }
         return ResponseEntity.notFound().build();
     }
+
+    @PostMapping
+    public ResponseEntity<Void> createCashCard(@RequestBody CashCard newCashCard, UriComponentsBuilder ucb) {
+        CashCard savedCashCard = cashCardRepository.save(newCashCard);
+        URI locationOfNewCashCard = ucb.path("cashcards/{id}")
+            .buildAndExpand(savedCashCard.id())
+            .toUri();
+        return ResponseEntity.created(locationOfNewCashCard).build();
+    }
+    
 }
